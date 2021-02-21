5b4870d318a6e43c475b64cf93bb44fe28efd54c
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-02-21 17:14:58 +0100

    changed folder name

---
+++ FA-18C/remote.lua
@@ -0,0 +1,251 @@
+-- Metadata
+
+local kb = libs.keyboard;
+
+-- Numpad
+
+actions.num0_up = function ()
+	 kb.up("ralt", "shift", "num0")
+end
+
+actions.num0_down = function ()
+	 kb.down("ralt", "shift", "num0")
+end
+
+actions.num1_up = function ()
+	 kb.up("ralt", "shift", "num1")
+end
+
+actions.num1_down = function ()
+	 kb.down("ralt", "shift", "num1")
+end
+
+actions.num2_up = function ()
+	 kb.up("ralt", "shift", "num2")
+end
+
+actions.num2_down = function ()
+	 kb.down("ralt", "shift", "num2")
+end
+
+actions.num3_up = function ()
+	 kb.up("ralt", "shift", "num3")
+end
+
+actions.num3_down = function ()
+	 kb.down("ralt", "shift", "num3")
+end
+
+actions.num4_up = function ()
+	 kb.up("ralt", "shift", "num4")
+end
+
+actions.num4_down = function ()
+	 kb.down("ralt", "shift", "num4")
+end
+
+actions.num5_up = function ()
+	 kb.up("ralt", "shift", "num5")
+end
+
+actions.num5_down = function ()
+	 kb.down("ralt", "shift", "num5")
+end
+
+actions.num6_up = function ()
+	 kb.up("ralt", "shift", "num6")
+end
+
+actions.num6_down = function ()
+	 kb.down("ralt", "shift", "num6")
+end
+
+actions.num7_up = function ()
+	 kb.up("ralt", "shift", "num7")
+end
+
+actions.num7_down = function ()
+	 kb.down("ralt", "shift", "num7")
+end
+
+actions.num8_up = function ()
+	 kb.up("ralt", "shift", "num8")
+end
+
+actions.num8_down = function ()
+	 kb.down("ralt", "shift", "num8")
+end
+
+actions.num9_up = function ()
+	 kb.up("ralt", "shift", "num9")
+end
+
+actions.num9_down = function ()
+	 kb.down("ralt", "shift", "num9")
+end
+
+actions.numENT_up = function ()
+	 kb.up("ralt", "shift", "retur")
+end
+
+actions.numENT_down = function ()
+	 kb.down("ralt", "shift", "retur")
+end
+
+actions.numCLR_up = function ()
+	 kb.up("ralt", "shift", "backspace")
+end
+
+actions.numCLR_down = function ()
+	 kb.down("ralt", "shift", "backspace")
+end
+
+-- ODU options
+
+actions.oduODU1_up = function ()
+	 kb.up("ralt", "shift", "digit1")
+end
+
+actions.oduODU1_down = function ()
+	 kb.down("ralt", "shift", "digit1")
+end
+
+actions.oduODU2_up = function ()
+	 kb.up("ralt", "shift", "digit2")
+end
+
+actions.oduODU2_down = function ()
+	 kb.down("ralt", "shift", "digit2")
+end
+
+actions.oduODU3_up = function ()
+	 kb.up("ralt", "shift", "digit3")
+end
+
+actions.oduODU3_down = function ()
+	 kb.down("ralt", "shift", "digit3")
+end
+
+actions.oduODU4_up = function ()
+	 kb.up("ralt", "shift", "digit4")
+end
+
+actions.oduODU4_down = function ()
+	 kb.down("ralt", "shift", "digit4")
+end
+
+actions.oduODU5_up = function ()
+	 kb.up("ralt", "shift", "digit5")
+end
+
+actions.oduODU5_down = function ()
+	 kb.down("ralt", "shift", "digit5")
+end
+
+-- Function row
+
+actions.funAP_up = function ()
+	 kb.up("ralt", "shift", "f1")
+end
+
+actions.funAP_down = function ()
+	 kb.down("ralt", "shift", "f1")
+end
+
+actions.funIFF_up = function ()
+	 kb.up("ralt", "shift", "f2")
+end
+
+actions.funIFF_down = function ()
+	 kb.down("ralt", "shift", "f2")
+end
+
+actions.funTCN_up = function ()
+	 kb.up("ralt", "shift", "f3")
+end
+
+actions.funTCN_down = function ()
+	 kb.down("ralt", "shift", "f3")
+end
+
+actions.funILS_up = function ()
+	 kb.up("ralt", "shift", "f4")
+end
+
+actions.funILS_down = function ()
+	 kb.down("ralt", "shift", "f4")
+end
+
+actions.funDL_up = function ()
+	 kb.up("ralt", "shift", "f5")
+end
+
+actions.funDL_down = function ()
+	 kb.down("ralt", "shift", "f5")
+end
+
+actions.funBCN_up = function ()
+	 kb.up("ralt", "shift", "f6")
+end
+
+actions.funBCN_down = function ()
+	 kb.down("ralt", "shift", "f6")
+end
+
+actions.funONOFF_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.funONOFF_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
+
+-- Radio controls and misc
+
+actions.radioCOM1UP_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.radioCOM1UP_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
+
+actions.radioCOM1DN_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.radioCOM1DN_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
+
+actions.radioCOM2UP_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.radioCOM2UP_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
+
+actions.radioCOM2DN_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.radioCOM2DN_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
+
+actions.miscIP_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.miscIP_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
+
+actions.miscEMCON_up = function ()
+	 kb.up("ralt", "shift", "f7")
+end
+
+actions.miscEMCON_down = function ()
+	 kb.down("ralt", "shift", "f7")
+end
---
+++ JF-17/remote.lua
@@ -0,0 +1,111 @@
+-- Metadata
+
+local kb = libs.keyboard;
+
+actions.num0 = function ()
+	 kb.stroke ("ralt", "num0")
+end
+
+actions.num1 = function ()
+	 kb.stroke ("ralt", "num1")
+end
+
+actions.num2 = function ()
+	 kb.stroke ("ralt", "num2")
+end
+
+actions.num3 = function ()
+	 kb.stroke ("ralt", "num3")
+end
+
+actions.num4 = function ()
+	 kb.stroke ("ralt", "num4")
+end
+
+actions.num5 = function ()
+	 kb.stroke ("ralt", "num5")
+end
+
+actions.num6 = function ()
+	 kb.stroke ("ralt", "num6")
+end
+
+actions.num7 = function ()
+	 kb.stroke ("ralt", "num7")
+end
+
+actions.num8 = function ()
+	 kb.stroke ("ralt", "num8")
+end
+
+actions.num9 = function ()
+	 kb.stroke ("ralt", "num9")
+end
+
+actions.funRTN = function ()
+	 kb.stroke ("ralt", "return")
+end
+
+actions.funL1 = function ()
+	 kb.stroke ("ralt", "f1")
+end
+
+actions.funL2 = function ()
+	 kb.stroke ("ralt", "f2")
+end
+
+actions.funL3 = function ()
+	 kb.stroke ("ralt", "f3")
+end
+
+actions.funL4 = function ()
+	 kb.stroke ("ralt", "f4")
+end
+
+actions.funR1 = function ()
+	 kb.stroke ("ralt", "f5")
+end
+
+actions.funR2 = function ()
+	 kb.stroke ("ralt", "f6")
+end
+
+actions.funR3 = function ()
+	 kb.stroke ("ralt", "f7")
+end
+
+actions.funR4 = function ()
+	 kb.stroke ("ralt", "f8")
+end
+
+actions.funOAP = function ()
+	 kb.stroke ("ralt", "1")
+end
+
+actions.funMRK = function ()
+	 kb.stroke ("ralt", "2")
+end
+
+actions.funPU = function ()
+	 kb.stroke ("ralt", "3")
+end
+
+actions.funHNS = function ()
+	 kb.stroke ("ralt", "4")
+end
+
+actions.funAP = function ()
+	 kb.stroke ("ralt", "5")
+end
+
+actions.funFPM = function ()
+	 kb.stroke ("ralt", "6")
+end
+
+actions.funPlus = function ()
+	 kb.stroke ("ralt", "numadd")
+end
+
+actions.funMinus = function ()
+	 kb.stroke ("ralt", "numsubtract")
+end
