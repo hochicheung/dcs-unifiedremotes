b163c052f02877b86b11af0995a9df2e0b4647ed
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-02-21 19:32:15 +0100

    finished all functions

--- Layout/remote.lua
+++ Layout/remote.lua
@@ -125,3 +125,359 @@ end
 actions.down_digit0 = function ()
 	 kb.down("ralt", "digit0")
 end
+
+-- Function
+actions.up_f1 = function ()
+	 kb.up("ralt", "f1")
+end
+actions.down_f1 = function ()
+	 kb.down("ralt", "f1")
+end
+actions.up_f2 = function ()
+	 kb.up("ralt", "f2")
+end
+actions.down_f2 = function ()
+	 kb.down("ralt", "f2")
+end
+actions.up_f3 = function ()
+	 kb.up("ralt", "f3")
+end
+actions.down_f3 = function ()
+	 kb.down("ralt", "f3")
+end
+actions.up_f4 = function ()
+	 kb.up("ralt", "f4")
+end
+actions.down_f4 = function ()
+	 kb.down("ralt", "f4")
+end
+actions.up_f5 = function ()
+	 kb.up("ralt", "f5")
+end
+actions.down_f5 = function ()
+	 kb.down("ralt", "f5")
+end
+actions.up_f6 = function ()
+	 kb.up("ralt", "f6")
+end
+actions.down_f6 = function ()
+	 kb.down("ralt", "f6")
+end
+actions.up_f7 = function ()
+	 kb.up("ralt", "f7")
+end
+actions.down_f7 = function ()
+	 kb.down("ralt", "f7")
+end
+actions.up_f8 = function ()
+	 kb.up("ralt", "f8")
+end
+actions.down_f8 = function ()
+	 kb.down("ralt", "f8")
+end
+actions.up_f9 = function ()
+	 kb.up("ralt", "f9")
+end
+actions.down_f9 = function ()
+	 kb.down("ralt", "f9")
+end
+actions.up_f10 = function ()
+	 kb.up("ralt", "f10")
+end
+actions.down_f10 = function ()
+	 kb.down("ralt", "f10")
+end
+actions.up_f11 = function ()
+	 kb.up("ralt", "f11")
+end
+actions.down_f11 = function ()
+	 kb.down("ralt", "f11")
+end
+actions.up_f12 = function ()
+	 kb.up("ralt", "f12")
+end
+actions.down_f12 = function ()
+	 kb.down("ralt", "f12")
+end
+
+-- Alphabetic
+actions.up_a = function ()
+	 kb.up("ralt", "a")
+end
+actions.down_a = function ()
+	 kb.down("ralt", "a")
+end
+actions.up_b = function ()
+	 kb.up("ralt", "b")
+end
+actions.down_b = function ()
+	 kb.down("ralt", "b")
+end
+actions.up_c = function ()
+	 kb.up("ralt", "c")
+end
+actions.down_c = function ()
+	 kb.down("ralt", "c")
+end
+actions.up_d = function ()
+	 kb.up("ralt", "d")
+end
+actions.down_d = function ()
+	 kb.down("ralt", "d")
+end
+actions.up_e = function ()
+	 kb.up("ralt", "e")
+end
+actions.down_e = function ()
+	 kb.down("ralt", "e")
+end
+actions.up_f = function ()
+	 kb.up("ralt", "f")
+end
+actions.down_f = function ()
+	 kb.down("ralt", "f")
+end
+actions.up_g = function ()
+	 kb.up("ralt", "g")
+end
+actions.down_g = function ()
+	 kb.down("ralt", "g")
+end
+actions.up_h = function ()
+	 kb.up("ralt", "h")
+end
+actions.down_h = function ()
+	 kb.down("ralt", "h")
+end
+actions.up_i = function ()
+	 kb.up("ralt", "i")
+end
+actions.down_i = function ()
+	 kb.down("ralt", "i")
+end
+actions.up_j = function ()
+	 kb.up("ralt", "j")
+end
+actions.down_j = function ()
+	 kb.down("ralt", "j")
+end
+actions.up_k = function ()
+	 kb.up("ralt", "k")
+end
+actions.down_k = function ()
+	 kb.down("ralt", "k")
+end
+actions.up_l = function ()
+	 kb.up("ralt", "l")
+end
+actions.down_l = function ()
+	 kb.down("ralt", "l")
+end
+actions.up_m = function ()
+	 kb.up("ralt", "m")
+end
+actions.down_m = function ()
+	 kb.down("ralt", "m")
+end
+actions.up_n = function ()
+	 kb.up("ralt", "n")
+end
+actions.down_n = function ()
+	 kb.down("ralt", "n")
+end
+actions.up_o = function ()
+	 kb.up("ralt", "o")
+end
+actions.down_o = function ()
+	 kb.down("ralt", "o")
+end
+actions.up_p = function ()
+	 kb.up("ralt", "p")
+end
+actions.down_p = function ()
+	 kb.down("ralt", "p")
+end
+actions.up_q = function ()
+	 kb.up("ralt", "q")
+end
+actions.down_q = function ()
+	 kb.down("ralt", "q")
+end
+actions.up_r = function ()
+	 kb.up("ralt", "r")
+end
+actions.down_r = function ()
+	 kb.down("ralt", "r")
+end
+actions.up_s = function ()
+	 kb.up("ralt", "s")
+end
+actions.down_s = function ()
+	 kb.down("ralt", "s")
+end
+actions.up_t = function ()
+	 kb.up("ralt", "t")
+end
+actions.down_t = function ()
+	 kb.down("ralt", "t")
+end
+actions.up_u = function ()
+	 kb.up("ralt", "u")
+end
+actions.down_u = function ()
+	 kb.down("ralt", "u")
+end
+actions.up_v = function ()
+	 kb.up("ralt", "v")
+end
+actions.down_v = function ()
+	 kb.down("ralt", "v")
+end
+actions.up_w = function ()
+	 kb.up("ralt", "w")
+end
+actions.down_w = function ()
+	 kb.down("ralt", "w")
+end
+actions.up_x = function ()
+	 kb.up("ralt", "x")
+end
+actions.down_x = function ()
+	 kb.down("ralt", "x")
+end
+actions.up_y = function ()
+	 kb.up("ralt", "y")
+end
+actions.down_y = function ()
+	 kb.down("ralt", "y")
+end
+actions.up_z = function ()
+	 kb.up("ralt", "z")
+end
+actions.down_z = function ()
+	 kb.down("ralt", "z")
+end
+
+-- Keypad Function
+actions.up_mult = function ()
+	 kb.up("ralt", "nummultiply")
+end
+actions.down_mult = function ()
+	 kb.down("ralt", "nummultiply")
+end
+actions.up_sub = function ()
+	 kb.up("ralt", "numsubtract")
+end
+actions.down_sub = function ()
+	 kb.down("ralt", "numsubtract")
+end
+actions.up_add = function ()
+	 kb.up("ralt", "numadd")
+end
+actions.down_add = function ()
+	 kb.down("ralt", "numadd")
+end
+actions.up_dec = function ()
+	 kb.up("ralt", "numdecimal")
+end
+actions.down_dec = function ()
+	 kb.down("ralt", "numdecimal")
+end
+actions.up_div = function ()
+	 kb.up("ralt", "numdivide")
+end
+actions.down_div = function ()
+	 kb.down("ralt", "numdivide")
+end
+actions.up_sep = function ()
+	 kb.up("ralt", "numseparator")
+end
+actions.down_sep = function ()
+	 kb.down("ralt", "numseparator")
+end
+
+-- Keyboard Nav
+actions.up_left = function ()
+	 kb.up("ralt", "left")
+end
+actions.down_left = function ()
+	 kb.down("ralt", "left")
+end
+actions.up_right = function ()
+	 kb.up("ralt", "right")
+end
+actions.down_right = function ()
+	 kb.down("ralt", "right")
+end
+actions.up_up = function ()
+	 kb.up("ralt", "up")
+end
+actions.down_up = function ()
+	 kb.down("ralt", "up")
+end
+actions.up_down = function ()
+	 kb.up("ralt", "down")
+end
+actions.down_down = function ()
+	 kb.down("ralt", "down")
+end
+actions.up_pgup = function ()
+	 kb.up("ralt", "pgup")
+end
+actions.down_pgup = function ()
+	 kb.down("ralt", "pgup")
+end
+actions.up_pgdown = function ()
+	 kb.up("ralt", "pgdown")
+end
+actions.down_pgdown = function ()
+	 kb.down("ralt", "pgdown")
+end
+actions.up_home = function ()
+	 kb.up("ralt", "home")
+end
+actions.down_home = function ()
+	 kb.down("ralt", "home")
+end
+actions.up_end = function ()
+	 kb.up("ralt", "end")
+end
+actions.down_end = function ()
+	 kb.down("ralt", "end")
+end
+actions.up_insert = function ()
+	 kb.up("ralt", "insert")
+end
+actions.down_insert = function ()
+	 kb.down("ralt", "insert")
+end
+actions.up_delete = function ()
+	 kb.up("ralt", "delete")
+end
+actions.down_delete = function ()
+	 kb.down("ralt", "delete")
+end
+actions.up_backspace = function ()
+	 kb.up("ralt", "backspace")
+end
+actions.down_backspace = function ()
+	 kb.down("ralt", "backspace")
+end
+actions.up_enter = function ()
+	 kb.up("ralt", "enter")
+end
+actions.down_enter = function ()
+	 kb.down("ralt", "enter")
+end
+actions.up_space = function ()
+	 kb.up("ralt", "space")
+end
+actions.down_space = function ()
+	 kb.down("ralt", "space")
+end
+actions.up_tab = function ()
+	 kb.up("ralt", "tab")
+end
+actions.down_tab = function ()
+	 kb.down("ralt", "tab")
+end
