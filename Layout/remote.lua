a2730f2b210c821e88818995179e4043dedd620b
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-02-21 18:51:30 +0100

    universal layout

---
+++ Layout/remote.lua
@@ -0,0 +1,127 @@
+-- Metadata
+
+local kb = require("keyboard");
+
+-- Numpad
+actions.up_num1 = function ()
+	 kb.up("ralt", "num1")
+end
+actions.down_num1 = function ()
+	 kb.down("ralt", "num1")
+end
+actions.up_num2 = function ()
+	 kb.up("ralt", "num2")
+end
+actions.down_num2 = function ()
+	 kb.down("ralt", "num2")
+end
+actions.up_num3 = function ()
+	 kb.up("ralt", "num3")
+end
+actions.down_num3 = function ()
+	 kb.down("ralt", "num3")
+end
+actions.up_num4 = function ()
+	 kb.up("ralt", "num4")
+end
+actions.down_num4 = function ()
+	 kb.down("ralt", "num4")
+end
+actions.up_num5 = function ()
+	 kb.up("ralt", "num5")
+end
+actions.down_num5 = function ()
+	 kb.down("ralt", "num5")
+end
+actions.up_num6 = function ()
+	 kb.up("ralt", "num6")
+end
+actions.down_num6 = function ()
+	 kb.down("ralt", "num6")
+end
+actions.up_num7 = function ()
+	 kb.up("ralt", "num7")
+end
+actions.down_num7 = function ()
+	 kb.down("ralt", "num7")
+end
+actions.up_num8 = function ()
+	 kb.up("ralt", "num8")
+end
+actions.down_num8 = function ()
+	 kb.down("ralt", "num8")
+end
+actions.up_num9 = function ()
+	 kb.up("ralt", "num9")
+end
+actions.down_num9 = function ()
+	 kb.down("ralt", "num9")
+end
+actions.up_num0 = function ()
+	 kb.up("ralt", "num0")
+end
+actions.down_num0 = function ()
+	 kb.down("ralt", "num0")
+end
+
+-- Digit
+actions.up_digit1 = function ()
+	 kb.up("ralt", "digit1")
+end
+actions.down_digit1 = function ()
+	 kb.down("ralt", "digit1")
+end
+actions.up_digit2 = function ()
+	 kb.up("ralt", "digit2")
+end
+actions.down_digit2 = function ()
+	 kb.down("ralt", "digit2")
+end
+actions.up_digit3 = function ()
+	 kb.up("ralt", "digit3")
+end
+actions.down_digit3 = function ()
+	 kb.down("ralt", "digit3")
+end
+actions.up_digit4 = function ()
+	 kb.up("ralt", "digit4")
+end
+actions.down_digit4 = function ()
+	 kb.down("ralt", "digit4")
+end
+actions.up_digit5 = function ()
+	 kb.up("ralt", "digit5")
+end
+actions.down_digit5 = function ()
+	 kb.down("ralt", "digit5")
+end
+actions.up_digit6 = function ()
+	 kb.up("ralt", "digit6")
+end
+actions.down_digit6 = function ()
+	 kb.down("ralt", "digit6")
+end
+actions.up_digit7 = function ()
+	 kb.up("ralt", "digit7")
+end
+actions.down_digit7 = function ()
+	 kb.down("ralt", "digit7")
+end
+actions.up_digit8 = function ()
+	 kb.up("ralt", "digit8")
+end
+actions.down_digit8 = function ()
+	 kb.down("ralt", "digit8")
+end
+actions.up_digit9 = function ()
+	 kb.up("ralt", "digit9")
+end
+actions.down_digit9 = function ()
+	 kb.down("ralt", "digit9")
+end
+actions.up_digit0 = function ()
+	 kb.up("ralt", "digit0")
+end
+actions.down_digit0 = function ()
+	 kb.down("ralt", "digit0")
+end
